67c6e6b342fa514057ddacdfe77520da991bad1f
Francesca <francesca.carletto.leon@gmail.com>
2018-09-13 15:59:17 -0400

    basic fuctionality, 2 levels
    movement (doesn't work super great), triggers working.

---
+++ SMASHYABITS/Assets/scripts/Char2Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char2Controller : MonoBehaviour
+{
+    public float jumpTimer = 0;
+
+    public float speed;
+    public float jumpPower;
+    private Rigidbody2D rigid;
+
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            Vector2 movement = new Vector2(moveHorizontal, 0);
+            rigid.AddForce(movement * speed);
+        }
+
+
+    }
+
+    private void Update()
+    {
+        jumpTimer -= Time.deltaTime;
+        if (Input.GetKey(KeyCode.W))
+        {
+            float moveVertical = Input.GetAxis("Vertical");
+            if (jumpTimer < 0 && moveVertical > 0)
+            {
+                rigid.velocity = new Vector2(0, 10);
+                //            rigid.AddForce(new Vector2(0, moveVertical * jumpPower));
+                jumpTimer = 2;
+            }
+        }
+
+    }
+
+}
---
+++ SMASHYABITS/Assets/scripts/CollisionController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionController : MonoBehaviour {
+
+    public GameObject winEffects;
+
+	// Use this for initialization
+	void Start () {
+        winEffects.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Trigger2")
+        {
+            Destroy(col.gameObject);
+            print("hit");
+            winEffects.transform.position = new Vector2(this.gameObject.transform.position.x, this.gameObject.transform.position.y);
+            winEffects.SetActive(true);
+        }
+    }
+}
