a09d8f844441ce798fd02527c6e5776a21484665
Francesca <francesca.carletto.leon@gmail.com>
2018-09-20 15:35:27 -0400

    additional level, tweaking movement

--- SMASHYABITS/Assets/scripts/Char1Controller.cs
+++ SMASHYABITS/Assets/scripts/Char1Controller.cs
@@ -13,35 +13,41 @@ public class Char1Controller : MonoBehaviour
 
     private void Start()
     {
-        rigid = GetComponent < Rigidbody2D>();
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.RightArrow)|| Input.GetKey(KeyCode.LeftArrow))
+        float moveHorizontal = 0;
+
+        if (Input.GetKey(KeyCode.A))
         {
-            float moveHorizontal = Input.GetAxis("Horizontal");
+            moveHorizontal = -1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveHorizontal = 1;
+        }
+    
             Vector2 movement = new Vector2(moveHorizontal, 0);
             rigid.AddForce(movement * speed);
-        }
+        
 
 
-        }
+    }
 
     private void Update()
     {
         jumpTimer -= Time.deltaTime;
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.W))
         {
-            float moveVertical = Input.GetAxis("Vertical");
-            if (jumpTimer < 0 && moveVertical > 0)
-                {
-                    rigid.velocity = new Vector2(0, 10);
-                    //            rigid.AddForce(new Vector2(0, moveVertical * jumpPower));
-                    jumpTimer = 2;
-                }
+            if (jumpTimer < 0)
+            {
+                rigid.velocity = new Vector2(0, 10);
+                jumpTimer = 1;
+            }
         }
-            
+
     }
 
 }
--- SMASHYABITS/Assets/scripts/Char2Controller.cs
+++ SMASHYABITS/Assets/scripts/Char2Controller.cs
@@ -13,41 +13,37 @@ public class Char2Controller : MonoBehaviour
 
     private void Start()
     {
-        rigid = GetComponent<Rigidbody2D>();
+        rigid = GetComponent < Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
         float moveHorizontal = 0;
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            moveHorizontal = -1;
-        }
-        else if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             moveHorizontal = 1;
         }
-    
-            Vector2 movement = new Vector2(moveHorizontal, 0);
-            rigid.AddForce(movement * speed);
-        
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveHorizontal = -1;
+        }
 
-
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rigid.AddForce(movement * speed);
     }
 
     private void Update()
     {
         jumpTimer -= Time.deltaTime;
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             if (jumpTimer < 0)
-            {
-                rigid.velocity = new Vector2(0, 10);
-                jumpTimer = 1;
-            }
+                {
+                    rigid.velocity = new Vector2(0, 10);
+                    jumpTimer = 1;
+                }
         }
-
+            
     }
 
 }
